35d970fe9dc3240b69c812bd207f9e5e8824c34d
LAPTOP-NQAKD3LO\Michael <wi21b006@technikum-wien.at>
2022-06-22 19:11:47 +0200

    api Time feature

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -2,6 +2,11 @@ package com.example.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
 public class TestApplication {
@@ -10,4 +15,9 @@ public class TestApplication {
         SpringApplication.run(TestApplication.class, args);
     }
 
+    @RequestMapping("/date")
+    public DateTimeFormatter date(@RequestParam String a){
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern(a);
+        return myFormatObj;
+    }
 }
